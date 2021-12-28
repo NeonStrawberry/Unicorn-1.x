33153b9b0e0b32678c216e360e9de7a8d48ac026
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2021-12-28 16:04:35 -0400

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/sockets.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "headers/httph.h"
+
+void handle(int fd) {
+    
+}
